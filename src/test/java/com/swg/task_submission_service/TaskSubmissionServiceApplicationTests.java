52e51ec8c12cfce9271de2f5614048698bca10fc
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-03-15 13:23:46 +0530

    init proj

---
+++ src/test/java/com/swg/task_submission_service/TaskSubmissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swg.task_submission_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSubmissionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
