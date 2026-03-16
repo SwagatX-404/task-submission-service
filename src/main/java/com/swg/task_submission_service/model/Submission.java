d8f839427c1fae61135ecc9f196c1dd3befb07b6
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-03-16 19:28:51 +0530

    feat: model submission

---
+++ src/main/java/com/swg/task_submission_service/model/Submission.java
@@ -0,0 +1,14 @@
+package com.swg.task_submission_service.model;
+
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Submission {
+
+    
+}
