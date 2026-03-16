27886c9c8e752ec0855c72c750bff7a473410792
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-03-16 19:19:00 +0530

    fix: minor

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -2,11 +2,13 @@ package controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @GetMapping
     public ResponseEntity<String> homeController() {
         return new ResponseEntity<>("Welcome to the Task Submission Service!", HttpStatus.OK);
     }
