2ecb3994b30569702fc7670b7eb591461c69e302
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-03-19 20:17:29 +0530

    feat: method decl

--- src/main/java/com/swg/task_submission_service/service/SubmissionService.java
+++ src/main/java/com/swg/task_submission_service/service/SubmissionService.java
@@ -1,6 +1,21 @@
 package com.swg.task_submission_service.service;
 
+import com.swg.task_submission_service.model.Submission;
+
+import java.util.List;
+
 public interface SubmissionService {
+
+    Submission submitTask(Long taskId, String githubLink, Long userId) throws Exception;
+
+    Submission getTaskSubmissionById(Long submissionId) throws Exception;
+
+    List<Submission> getAllTaskSubmissions();
+
+    List<Submission> getTaskSubmissionByTaskId(Long taskId);
+
+    Submission acceptDeclineSubmission(Long id, String status) throws Exception;
+
 
 
 }
