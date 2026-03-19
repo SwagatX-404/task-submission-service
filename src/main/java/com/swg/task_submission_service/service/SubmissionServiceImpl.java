d1679a958b75ff1d70a2056189a31819a9ba3c64
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-03-19 20:28:19 +0530

    fix: min changes

--- src/main/java/com/swg/task_submission_service/service/SubmissionServiceImpl.java
+++ src/main/java/com/swg/task_submission_service/service/SubmissionServiceImpl.java
@@ -1,10 +1,20 @@
 package com.swg.task_submission_service.service;
 
 import com.swg.task_submission_service.model.Submission;
+import com.swg.task_submission_service.repository.SubmissionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
+@Service
 public class SubmissionServiceImpl implements SubmissionService {
+
+    @Autowired
+    private SubmissionRepository submissionRepository;
+
+
     @Override
     public Submission submitTask(Long taskId, String githubLink, Long userId) throws Exception {
         return null;
