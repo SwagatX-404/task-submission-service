29a97acbe0164c2102640a45c0c03a841547dc92
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-03-19 20:27:34 +0530

    feat: SubmissionRepository extends JpaRepository

---
+++ src/main/java/com/swg/task_submission_service/repository/SubmissionRepository.java
@@ -0,0 +1,11 @@
+package com.swg.task_submission_service.repository;
+
+import com.swg.task_submission_service.model.Submission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SubmissionRepository extends JpaRepository<Submission, Long> {
+
+    List<Submission> findByTaskId(Long taskId);
+}
