6df209c72cc1284a81e38214153d62ad32d7fd69
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-03-20 22:16:26 +0530

    feat: taskdto model

---
+++ src/main/java/com/swg/task_submission_service/model/TaskDto.java
@@ -0,0 +1,26 @@
+package com.swg.task_submission_service.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskDto {
+
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private String image;
+
+    private Long assignedUserId;
+
+    private List<String> tags=new ArrayList<>();
+
+    private TaskStatus status;
+
+    private LocalDateTime deadline;
+
+    private LocalDateTime createdAt;
+}
