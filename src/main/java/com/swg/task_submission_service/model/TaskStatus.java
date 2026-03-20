dfba8deb98e0ca31a85794492f6614e2033190be
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-03-20 22:17:43 +0530

    feat: enum TaskStatus

---
+++ src/main/java/com/swg/task_submission_service/model/TaskStatus.java
@@ -0,0 +1,17 @@
+package com.swg.task_submission_service.model;
+
+public enum TaskStatus {
+
+
+    PENDING(  "PENDING"),
+
+    ASSIGNED("ASSIGNED"),
+
+    DONE("DONE");
+
+
+    TaskStatus(String done ){
+
+    }
+
+}
