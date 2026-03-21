6d98dfc8985378ae1b8dd130e962998590712de9
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-03-21 19:17:02 +0530

    feat: method impl submittask, gettasksubmitbyid

--- src/main/java/com/swg/task_submission_service/service/SubmissionServiceImpl.java
+++ src/main/java/com/swg/task_submission_service/service/SubmissionServiceImpl.java
@@ -1,10 +1,12 @@
 package com.swg.task_submission_service.service;
 
 import com.swg.task_submission_service.model.Submission;
+import com.swg.task_submission_service.model.TaskDto;
 import com.swg.task_submission_service.repository.SubmissionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -14,15 +16,28 @@ public class SubmissionServiceImpl implements SubmissionService {
     @Autowired
     private SubmissionRepository submissionRepository;
 
+    @Autowired
+    private TaskService taskService;
+
 
     @Override
-    public Submission submitTask(Long taskId, String githubLink, Long userId) throws Exception {
-        return null;
+    public Submission submitTask(Long taskId, String githubLink, Long userId, String jwt) throws Exception {
+        TaskDto task = taskService.getTaskById(taskId, jwt);
+        if(task!=null){
+            Submission submission = new Submission();
+            submission.setTaskId(taskId);
+            submission.setUserId(userId);
+            submission.setGithubLink(githubLink);
+            submission.setSubmissionTime(LocalDateTime.now());
+            return submissionRepository.save(submission);
+        }
+        throw new Exception("Task not found");
     }
 
     @Override
     public Submission getTaskSubmissionById(Long submissionId) throws Exception {
-        return null;
+        return submissionRepository.findById(submissionId)
+                .orElseThrow(() -> new Exception("Submission not found for id: " + submissionId));
     }
 
     @Override
