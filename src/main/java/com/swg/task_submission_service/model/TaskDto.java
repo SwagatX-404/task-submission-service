0f714510545a2f303a0e263680e8e001370679c9
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-03-21 19:14:05 +0530

    fix: @Annotation

--- src/main/java/com/swg/task_submission_service/model/TaskDto.java
+++ src/main/java/com/swg/task_submission_service/model/TaskDto.java
@@ -1,9 +1,17 @@
 package com.swg.task_submission_service.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class TaskDto {
 
     private Long id;
