a6efdccb4768e48278151644f4edde0475e10b30
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-03-21 19:15:47 +0530

    feat: interface user service

---
+++ src/main/java/com/swg/task_submission_service/service/UserService.java
@@ -0,0 +1,16 @@
+package com.swg.task_submission_service.service;
+
+
+import com.swg.task_submission_service.model.UserDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name="USER-SERVICE", url = "http://localhost:5001/")
+public interface UserService {
+
+    @GetMapping("/api/users/profile")
+    public UserDto getUserProfile(
+            @RequestHeader ("Authorization") String jwt
+    ) throws Exception;
+}
