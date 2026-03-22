a0b199bf168fcfe88226d73e956d5f0a7e099c85
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-03-22 21:52:43 +0530

    feat: SubmissionController

---
+++ src/main/java/com/swg/task_submission_service/controller/SubmissionController.java
@@ -0,0 +1,13 @@
+package com.swg.task_submission_service.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/submissions")
+public class SubmissionController {
+
+
+    
+}
