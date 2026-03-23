f5a4210f545e866952e2c0ff053b250afe9977b7
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-03-23 23:57:49 +0530

    feat: get submission by id

--- src/main/java/com/swg/task_submission_service/controller/SubmissionController.java
+++ src/main/java/com/swg/task_submission_service/controller/SubmissionController.java
@@ -9,10 +9,7 @@ import com.swg.task_submission_service.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/submissions")
@@ -27,6 +24,7 @@ public class SubmissionController {
     @Autowired
     private TaskService taskService;
 
+    @PostMapping()
     public ResponseEntity<Submission> submitTask(
 
             @RequestParam Long taskId,
@@ -35,6 +33,17 @@ public class SubmissionController {
     ) throws  Exception{
         UserDto user = userService.getUserProfile(jwt);
         Submission submission = submissionService.submitTask(taskId, github_link, user.getId(), jwt);
+        return new ResponseEntity<>(submission, HttpStatus.CREATED);
+    }
+
+    @GetMapping( "/{id}")
+    public ResponseEntity<Submission> getSubmissionById (
+            @PathVariable Long id,
+             @RequestHeader ("Authorization") String jwt
+
+    ) throws  Exception{
+        UserDto user = userService.getUserProfile(jwt);
+        Submission submission = submissionService.getTaskSubmissionById(id);
         return new ResponseEntity<>(submission, HttpStatus.CREATED);
     }
 
